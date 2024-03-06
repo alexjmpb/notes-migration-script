6361d31ee61abf019bae86ec898631fe55ccf7f3
Alexander Mendoza <62396424+alexjmpb@users.noreply.github.com>
2024-03-06 16:17:07 -0500

    Update migrate_ccx_notes.py

--- migrate_ccx_notes.py
+++ migrate_ccx_notes.py
@@ -1,31 +1,24 @@
-# myapp/management/commands/migrate_ccx_keys.py
-from django.core.management.base import BaseCommand
+
 from ccx_keys.locator import CCXLocator
 from notesapi.v1.models import Note
 
-class Command(BaseCommand):
-    help = 'Migrate CCX keys in notes.'
+def get_key_without_branch(ccx_id):
+    return str(CCXLocator.from_string(ccx_id).for_branch(None))
 
-    def add_arguments(self, parser):
-        parser.add_argument('--dry-run', action='store_true', help='Perform a dry run without making changes.')
 
-    def handle(self, *args, **options):
-        self.migrate_ccx_keys(dry_run=options['dry_run'])
+def migrate_ccx_keys(dry_run=False):
+    damaged_notes = Note.objects.filter(course_id__contains="branch")
 
-    def get_key_without_branch(self, ccx_id):
-        return str(CCXLocator.from_string(ccx_id).for_branch(None))
+    notes_to_update = []
+    for damaged_note in damaged_notes:
+        versioned_course_id = damaged_note.course_id
+        damaged_note.course_id = get_key_without_branch(versioned_course_id)
+        notes_to_update.append(damaged_note)
 
-    def migrate_ccx_keys(self, dry_run=False):
-        damaged_notes = Note.objects.filter(course_id__contains="branch")
+    if dry_run:
+        print(f'Dry run: Changes that would be made')
+        print(f'Dry run: entries from {notes_to_update[0].id} to {notes_to_update[-1].id}')
+    else:
+        Note.objects.bulk_update(notes_to_update, ['course_id'], batch_size=500)
 
-        notes_to_update = []
-        for damaged_note in damaged_notes:
-            versioned_course_id = damaged_note.course_id
-            damaged_note.course_id = self.get_key_without_branch(versioned_course_id)
-            notes_to_update.append(damaged_note)
-
-        if dry_run and notes_to_update:
-            self.stdout.write('Dry run: Changes that would be made')
-            self.stdout.write(f'Dry run: entries from {notes_to_update[0].id} to {notes_to_update[-1].id}')
-        elif notes_to_update:
-            Note.objects.bulk_update(notes_to_update, ['course_id'], batch_size=500)
+print('test')
